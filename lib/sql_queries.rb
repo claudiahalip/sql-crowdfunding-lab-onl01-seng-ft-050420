c06b7607a8fb5460e596f6f511853a42e613cba5
claudia v <claudia4ro@yahoo.com>
2020-06-03 02:11:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,6 +20,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, pledges.amount FROM users INNER JOIN pledges ON users.id = pledges.user_id ORDER BY pledges.amount "
+end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "Write your SQL query Here"
